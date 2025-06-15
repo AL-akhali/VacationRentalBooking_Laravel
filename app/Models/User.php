ae3f8bc0a89d188c2a3f3e6c9199097001fccdcd
AnasALAkhali <alakhalianas@gmail.com>
2025-06-15 12:55:43 +0300

    Add user roles (host, guest), define relationship with properties, update factory and seeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
@@ -44,5 +45,16 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // التحقق من الدور
+    public function isHost(): bool
+    {
+        return $this->role === 'host';
+    }
+
+    public function isGuest(): bool
+    {
+        return $this->role === 'guest';
     }
 }
